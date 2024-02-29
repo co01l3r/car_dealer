91fb74b69b186a33559a07af952637a903e93dea
theRealCooller <jakublohnicky@hotmail.com>
2024-02-29 18:26:18 +0100

    basic project structure

--- car_dealer/settings.py
+++ car_dealer/settings.py
@@ -139,6 +139,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+TESTS = 'dealer.tests'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
